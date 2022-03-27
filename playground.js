53bcc716537e307f4114c70458b2579bfb45cf7d
alnavski98 <alnavski98@gmail.com>
2022-03-27 03:47:37 +0200

    added js

--- playground.js
+++ playground.js
@@ -1,3 +1,5 @@
+let count = 0;
+
 function myFunction() {
     let a = "UwU ";
     let b = "what's this?";
@@ -5,3 +7,14 @@ function myFunction() {
     alert(c);
 }
 
+function incrementer() {
+    //alert(count++);
+    //console.log("Clicked");
+    count++
+    document.getElementById('increment').value = count
+} 
+
+function reset() {
+    count = 0
+    document.getElementById('increment').value = count
+}
