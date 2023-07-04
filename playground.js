5a606d963fa9ebf19440345d701770d40f582ed9
Alexander Navasca Skinstad <alnavski98@gmail.com>
2023-07-04 23:57:58 +0200

    Added a simple clock

--- playground.js
+++ playground.js
@@ -34,15 +34,15 @@ function reset() {
 function time() {
     setInterval(time, 1000);
     var date = new Date()
-
+    window.onload = 
     hours = date.getHours()
     minutes = date.getMinutes()
     seconds = date.getSeconds()
 
-    document.getElementById('time').innerHTML = "The time is currently: " 
-                                            + (hours < 10 ? "0" : "") + hours 
-                                            + ":" + (minutes < 10 ? "0" : "") + minutes 
-                                            + ":" + (seconds < 10 ? "0" : "") + seconds
+    document.getElementById('time').innerHTML = (hours < 10 ? "0" : "") + hours 
+                                                + ":" + (minutes < 10 ? "0" : "") + minutes 
+                                                + ":" + (seconds < 10 ? "0" : "") + seconds
+                                            
 }
 
 function displayName() {
@@ -50,4 +50,19 @@ function displayName() {
     aozora.introduceSelf()
 }
 
-
+window.addEventListener('DOMContentLoaded', () => {
+    const timeElement = document.getElementById('time');
+  
+    // Function to update the time
+    const updateTime = () => {
+      const date = new Date();
+      const timeString = date.toLocaleTimeString();
+      timeElement.textContent = timeString;
+    };
+  
+    // Call the function to update the time immediately
+    updateTime();
+  
+    // Update the time every second
+    setInterval(updateTime, 1000);
+  });
